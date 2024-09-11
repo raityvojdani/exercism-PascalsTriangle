8c55d3e7c83511d8355ff5f31b068e52a3576b47
Emertat <Emertat@RAITY>
2024-09-11 17:41:51 +0330

    Add project files.

---
+++ PascalsTriangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static  class PascalsTriangle
+{
+    public static IEnumerable<IEnumerable<int>> Calculate(int rows)
+    {
+        // A list to hold all rows of List<List<int>> type 
+        List<List<int>> result = new List<List<int>>();
+
+        // Loops to generate rows
+        for (int i = 0; i < rows; i++)
+        {
+            // Each line is a new list
+            List<int> row = new List<int>();
+
+            // The first number of each row is always 1
+            row.Add(1);
+
+            // If the row has more than 2 elements, the middle elements should be calculated
+            for (int j = 1; j < i; j++)
+            {
+                int previousRowLeft = result[i - 1][j - 1];
+                int previousRowRight = result[i - 1][j];
+                row.Add(previousRowLeft + previousRowRight);
+            }
+
+            //The last number of each line is always 1 (except the first line).
+            if (i > 0)
+            {
+                row.Add(1);
+            }
+
+            //Add row to the main list
+            result.Add(row);
+        }
+
+        return result;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        //There is no need to create an object from PascalsTriangle, because the class and method are static.
+        IEnumerable<IEnumerable<int>> result = PascalsTriangle.Calculate(3);
+
+        //You must use foreach to access the result values ​​because result is an IEnumerable.
+        foreach (IEnumerable<int> row in result)
+        {
+            
+            foreach (int num in row)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine(); //To create a new line after each row
+        }
+
+    }
+}
