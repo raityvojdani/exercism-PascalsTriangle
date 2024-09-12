479b96b2eec7735504c4f6e76a535aeb566c443a
raity <82751237+raityvojdani@users.noreply.github.com>
2024-09-12 16:13:41 +0330

    Update PascalsTriangle.cs

--- PascalsTriangle.cs
+++ PascalsTriangle.cs
@@ -1,23 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-
-public static  class PascalsTriangle
+/// <summary>
+/// This class provides a method to calculate Pascal's Triangle up to a specified number of rows.
+/// </summary>
+public class PascalsTriangle
 {
-    public static IEnumerable<IEnumerable<int>> Calculate(int rows)
+
+    public PascalsTriangle(){}
+
+    public IEnumerable<IEnumerable<int>> Calculate(int rows)
     {
-        // A list to hold all rows of List<List<int>> type 
+        return GeneratePascalsTriangle(rows);
+    }
+
+    private List<List<int>> GeneratePascalsTriangle(int rows)
+    {
         List<List<int>> result = new List<List<int>>();
 
-        // Loops to generate rows
         for (int i = 0; i < rows; i++)
         {
-            // Each line is a new list
             List<int> row = new List<int>();
-
-            // The first number of each row is always 1
             row.Add(1);
 
-            // If the row has more than 2 elements, the middle elements should be calculated
             for (int j = 1; j < i; j++)
             {
                 int previousRowLeft = result[i - 1][j - 1];
@@ -25,16 +29,15 @@ public static  class PascalsTriangle
                 row.Add(previousRowLeft + previousRowRight);
             }
 
-            //The last number of each line is always 1 (except the first line).
             if (i > 0)
             {
                 row.Add(1);
             }
 
-            //Add row to the main list
             result.Add(row);
         }
 
         return result;
     }
+
 }
